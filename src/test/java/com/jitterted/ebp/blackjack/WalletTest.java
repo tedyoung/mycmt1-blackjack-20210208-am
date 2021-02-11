564db905dcba6771dfe1cc2de742c3906eda7b31
Ted M. Young <tedyoung@gmail.com>
2021-02-11 10:22:51 -0800

    Wallet with isEmpty, balance, and addMoney "happy cases".

---
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -0,0 +1,61 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class WalletTest {
+  @Test
+  public void newWalletIsEmpty() throws Exception {
+    Wallet wallet = new Wallet();
+
+    assertThat(wallet.isEmpty())
+        .isTrue();
+  }
+
+  @Test
+  public void newWalletAddMoneyIsNotEmpty() throws Exception {
+    //given a new wallet
+    Wallet wallet = new Wallet();
+
+    //when we add money
+    wallet.addMoney(10);
+
+    //then we expect it to not be empty
+    assertThat(wallet.isEmpty())
+        .isFalse();
+  }
+
+  @Test
+  public void newWalletThenBalanceIsZero() throws Exception {
+    Wallet wallet = new Wallet();
+
+    // THEN balance is ZERO
+    assertThat(wallet.balance())
+        .isZero();
+  }
+
+  @Test
+  public void newWalletAdd15ThenBalanceIs15() throws Exception {
+    Wallet wallet = new Wallet();
+
+    wallet.addMoney(15);
+
+    assertThat(wallet.balance())
+        .isEqualTo(15);
+  }
+
+  @Test
+  public void newWalletAdd17AndAdd18ThenBalanceIs35() throws Exception {
+    Wallet wallet = new Wallet();
+    wallet.addMoney(17);
+
+    wallet.addMoney(18);
+
+    assertThat(wallet.balance())
+        .isEqualTo(17 + 18);
+  }
+
+
+
+}
